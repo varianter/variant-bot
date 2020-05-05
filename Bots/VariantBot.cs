46f0b58f1c0631dafdee9ca109977e9603c3f7e7
Ole J. Skogstad <jorgen.skogstad@gmail.com>
2020-05-05 14:40:42 +0200

    Remove OnMembersAddedAsync

--- Bots/VariantBot.cs
+++ Bots/VariantBot.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
@@ -19,16 +18,6 @@ namespace VariantBot.Bots
         protected override Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
-        }
-
-        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
-        {
-            foreach (var member in membersAdded)
-            {
-                if (member.Id == turnContext.Activity.Recipient.Id) continue;
-                var welcomeText = $"Hello, {member.Name}/{member.Id} ,and welcome!";
-                await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
-            }
         }
     }
 }
