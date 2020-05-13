be5bb1fa5806e2348da83bc00cb59912275153cc
Ole J. Skogstad <skogstad@softis.net>
2020-05-13 13:25:13 +0200

    Add Info class for fetching environment vars

---
+++ Slack/Info.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace VariantBot.Slack
+{
+    public static class Info
+    {
+        public static string WifiSSIDAndPassword =>
+            Environment.GetEnvironmentVariable("VARIANT_WIFI_SSID_AND_PASSWORD");
+    }
+}
